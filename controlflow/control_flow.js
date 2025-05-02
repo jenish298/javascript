86cbd6cddb9cf781f0f8e4c43bca2a20d417fad2
jenish bhadani <jenishbhadani9@gmail.com>
2025-05-01 23:26:42 -0400

    message

---
+++ controlflow/control_flow.js
@@ -0,0 +1,36 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+
+
+console.log("Access Level:", accessLevel);
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
